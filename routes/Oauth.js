0fbab7736b26cb828fcd8e7a0de43b5346708c67
Zachary Smith <z.smith32@gmail.com>
2017-02-05 13:02:39 -0800

    updating oauth for production

--- routes/Oauth.js
+++ routes/Oauth.js
@@ -8,17 +8,10 @@ router.get('/facebook', passport.authenticate('facebook',
 	{ scope: ['email'] }
 	));
 
-// router.get('/facebook/callback', function(req, res, next) {
-// 	console.log('facebook call back');
-// 	res.redirect('/signup');
-// })
-
 router.get('/facebook/callback', passport.authenticate('facebook', 
 	{ 	session: false,
 		failureRedirect: '/login' }),
 	function(req, res) {
-		// var token = req.user.my_token;
-		// console.log('callback route token:', token);
 		console.log('req in callback route',req.user);
 		res.cookie('JWT', req.user.my_token);
 		res.redirect('/dashboard');
