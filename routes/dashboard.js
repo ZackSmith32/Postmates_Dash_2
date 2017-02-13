d1aa8b12e517a2eb56445c2b5db0dd5e89b4ab89
Zachary Smith <z.smith32@gmail.com>
2017-02-12 23:10:36 -0800

    writing function to generate data for heat map

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,9 +6,12 @@ var Jobs = require('../models/jobs.js')
 var passport = require('passport');
 var Users = require('../models/users');
 var jwt = require('jsonwebtoken');
+var fs = require('fs');
 
 var jwtAuth = passport.authenticate('jwt', 
 	{ session: false, failureRedirect: '/login' });
+
+var heat_map = fs.readFileSync('./heat_data.tsv', 'utf8')
 
 router.get('/', jwtAuth, function(req, res, next) {
 	var user
@@ -18,6 +21,7 @@ router.get('/', jwtAuth, function(req, res, next) {
 		user = req.user.email;
 	}
 	allJobs(user)
+		.then(function(result) {return heat_data(result)})
 		.then(function(allJobs) {
 			console.log(user);
 			res.render('dashboard', { 
@@ -42,4 +46,71 @@ function allJobs(user) {
 	})
 }
 
+function heat_data(data) {
+	console.log('in heat format func');
+	var heat_data = [['day', 'time', 'payout', 'tips', 'total', 'time', 'rate']]
+	return new Promise( function(resolve, reject) {
+		for (var day = 1; day <= 7; day++) {
+			for (var time = 1; time <= 24; time++) {
+				var filtered = data.filter(function(job) {
+					// console.log(job.jobStart.getDay())
+					return 	job.jobStart.getDay() == day &&
+							job.jobStart.getHours() == time
+				})
+				var payout = 0;
+				var tips = 0;
+				var time = 0;
+				for (job of filtered) {
+					tips += job.jobTip;
+					payout += job.payout;
+					time += job.jobLengthHours;
+				}
+				var total = payout + tips;
+				var rate = total / time;
+				console.log(filtered);
+				heat_data.push([day, time, 0, tips, payout + tips, time, rate]);
+			}
+		}
+		console.log('heat data', heat_data)
+		resolve(heat_data);
+	})
+}
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
